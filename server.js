bf5ad02f090e4fb3123014308c0eb4d9d9d106cc
dannywamuya <dannywamuya@gmail.com>
2021-11-17 09:51:31 +0300

    update content

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const port = 8000;
+const port = 8000 || process.env.PORT;
 
 app.use('/res', express.static(path.resolve(__dirname, 'src', 'res')));
 app.use('/static', express.static(path.resolve(__dirname, 'src', 'static')));
